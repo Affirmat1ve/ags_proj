a29ed3ff4d2fc07ef3bb873de525ae7e19c06a64
Affirmative <himka-00@mail.ru>
2026-03-13 11:23:31 +0300

    config class added

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+from pathlib import Path
 
+
+class Config:
+    def __init__(self, config_path = 'config.json'):
+        self._data = {}
+        self._load(config_path)
+
+    def _load(self, config_path):
+        path = Path(config_path)
+
+        if not path.exists():
+            raise FileNotFoundError(f"Config file not found: {config_path}")
+
+        with open(path, 'r', encoding='utf-8') as f:
+            self._data = json.load(f)
+
+    def __getitem__(self, key):
+        return self._data[key]
+
+
+config = Config()
 
 def get_requester_page(url):
     response = requests.get(url)
@@ -14,7 +35,7 @@ def get_comments(page):
     page_title = page.find('title')
     comment_tree = BeautifulSoup(str(page.find_all('div', class_='tm-comments-wrapper__inner')), 'html5lib')
     text_array = [x.get_text() for x in comment_tree.find_all('div', class_='tm-comment__body-content')]
-    return {'Article': page_title.string, 'comment_texts': text_array}
+    return {'Article': page_title.string, 'Comment_texts': text_array}
 
 
 def dump_json(data):
@@ -23,7 +44,6 @@ def dump_json(data):
 
 
 if __name__ == '__main__':
-    url = 'https://habr.com/ru/articles/346198/comments/'
-    web_page = get_requester_page(url)
+    web_page = get_requester_page(url = config['url'])
     comment_data = get_comments(web_page)
     dump_json(comment_data)
